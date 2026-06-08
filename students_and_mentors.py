5db6267f7bdfca0e7207aa56f360ad0d0ee6f1b1
Александр Денисов <i@saintalex.ru>
2026-06-08 22:42:14 +0400

    Task#3. Add new methods and logics

--- students_and_mentors.py
+++ students_and_mentors.py
@@ -1,19 +1,19 @@
 class Student:
-    
-    def __init__(self, name, surname, gender):
+     
+    def __init__(self, name, surname, gender): # инициализация
         self.name = name
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-    
+          
     def rate_lecture(self, lecturer, course, grade):
-        
+        '''Метод выставления оценки лекторам'''            
         if (not isinstance(lecturer, Lecturer) 
             or not isinstance(grade, int) or grade < 1 or grade > 10):
             return 'Ошибка'
-        
+             
         if (course in self.courses_in_progress
             and course in lecturer.courses_attached):
             if course in lecturer.grades:
@@ -22,25 +22,60 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+              
+    def __str__(self): # Приводим отображение к более удобному выводу
+        avg_grade = self.calc_average_grade()
+        courses_in_progress = ', '.join(self.courses_in_progress)
+        finished_courses = ', '.join(self.finished_courses)
         
+        return (f'Имя:{self.name}\n'
+                f'Фамилия:{self.surname}\n'
+                f'Средняя оценка за домашнее задание:{avg_grade:.1f}\n'
+                f'Курсы в процессе изучения:{courses_in_progress}\n'
+                f'Завершенные курсы:{finished_courses}')
+    
+    def calc_average_grade(self):
+        '''Метод подсчета средней оценки за домашние задания'''
+        if not self.grades:
+            return 0.0
+        all_grades = []
+        for grades_list in self.grades.values():
+            all_grades.extend(grades_list)
+            return sum(all_grades) / len(all_grades) if all_grades else 0.0
+    
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return 'Ошибка сравнения'
+        return self.calc_average_grade() < other.calc_average_grade()
+    
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            return 'Ошибка сравнения'
+        return self.calc_average_grade() == other.calc_average_grade()
+          
 class Mentor:
     
-    def __init__(self, name, surname):
+    def __init__(self, name, surname): # Инициализация
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
 class Reviewer(Mentor):
-    
+        
     def rate_hw(self, student, course, grade):
+        '''Метод выставления оценки студентам курса''' 
         if (isinstance(student, Student) and course
-        in self.courses_attached and course in student.courses_in_progress):
+            in self.courses_attached and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+        
+    def __str__(self): # Приводим отображение к более удобному выводу
+        return (f'Имя: {self.name}\n'
+                f'Фамилия {self.surname}')
 
 class Lecturer(Mentor):
     
@@ -48,17 +83,27 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
- 
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
- 
-print(student.rate_lecture(lecturer, 'Python', 7))   # None
-print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
- 
-print(lecturer.grades)  # {'Python': [7]}  
+    def calc_average_grade(self):
+        '''Метод подсчета средней оценки от студентов по курсу'''
+        if not self.grades:
+            return 0.0
+        all_grades = []
+        for grades_list in self.grades.values():
+            all_grades.extend(grades_list)
+        return sum(all_grades) / len(all_grades) if all_grades else 0.0
+        
+    def __str__(self):
+        avg_grade = self.calc_average_grade()
+        return (f'Имя:{self.name}\n'
+                f'Фамилия:{self.surname}\n'
+                f'Средняя оценка за лекции:{avg_grade:.1f}')    
+    
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return 'Ошибка сравнения'
+        return self.calc_average_grade() < other.calc_average_grade()
+    
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            return 'Ошибка сравнения'
+        return self.calc_average_grade() == other.calc_average_grade()
